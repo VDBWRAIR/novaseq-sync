a4261004f5fb13f88f471b76f3fd05765ababdb7
averagehat <bigmwp@gmail.com>
2019-07-10 21:25:05 +0000

    tests working and passing

--- newsync.py
+++ newsync.py
@@ -1,11 +1,9 @@
 import sh
-import date
 import csv
-import date
-from path import Path
+from datetime import date
 import os
-
-
+from path import Path
+import itertools 
 # fails on py3 because map gets drained as a generator
 # TODO: we now have three versions of the data. the bcl, the gzipped, and the uncompressed fastqs.
 # we should elminate one of those, probably. just delete the gzipped later, probably.
@@ -16,7 +14,7 @@ def rename_issue_date(rbs, sname, sid, inpath): # -> str
     # (Path, str, int) ->  Path
     id_basename = Path(inpath.basename().replace(sname, str(sid)))
     today = date.today().isoformat().replace('-','_')
-    new_basename = "{}_{}".format(id_basename.stripext().stripext(), today)).with_suffix('.fastq')
+    new_basename = Path("{}_{}".format(id_basename.stripext().stripext(), today)).with_suffix('.fastq')
     return rbs / str(sid) / new_basename
 
 def flatten(*seq):
@@ -27,19 +25,27 @@ def newnames_by_dict(indir, rbs, snames, sids):
   def get_zipped(samp):
     # path -> [path]
     return indir.glob(samp + '*') # side-effect :(
+
   fastq_groups = map(get_zipped, snames) # [[path]]
-  new_names = map(rename_issue_date, to_rename, snames, sids) 
-  
+
   # [(Path, path)]
   old_and_new = flatten([ map(lambda f: (f, rename_issue_date(rbs, sname, sid, f)), fastqs) \
         for fastqs, sname, sid in zip(fastq_groups, snames, sids) ]) 
   return old_and_new
 
+def mkdir_p(p):
+    if not os.path.exists(p):
+        os.mkdir(p)
+
 def run(indir, rbs, id_lst):
-  names_and_ids = list(csv.DictReader(open('id.lst'), delimiter=',')) 
-  snames, sids = zip(*names_and_ids.items())
+  assert hasattr(indir, 'mkdir_p')
+  assert hasattr(rbs, 'mkdir_p')
+  assert hasattr(id_lst, 'mkdir_p')
+  names_and_ids = list(csv.DictReader(open(id_lst), delimiter=',')) 
+  snames, sids = zip(*[ (v['SampleName'], v['IssueID']) for v in names_and_ids])
   for sid in sids:
-    (rbs / sid).mkdir_p()
+    #(rbs / sid).mkdir_p()
+    mkdir_p(rbs / sid)
   old_and_new = newnames_by_dict(indir, rbs, snames, sids) 
   for old, new in old_and_new:
     sh.gunzip(old)
--- test_newsync.py
+++ test_newsync.py
@@ -5,14 +5,18 @@ from mock import patch
 import shutil 
 import sh
 from path import Path 
-import date
+from datetime import date
+import os
+import newsync 
+from collections import Counter
+from nose.tools import eq_, ok_
 
 ids = [21211 ,21212 ,21213 ,21214 ,21215 ,21216 ,21217 ,21218 ,20054 ,20057 ,20058 ,20063 ,20065 ,20066 ,20067 ]
+ids_strs = map(str, ids)
+samps =  ['00147BLA001xQ' , '00150BLA001xQ' , '00537BLA001xQ' , '00006BLA004xQ' , '00043BLA001xQ' , '00170BLA006xQ' , '00241BLA001xQ' , '00813BLA008xQ' , '00034BLA003' , '00165BLA002' , '00192BLA001' , '00542BLA001' , '00622BLA001' , '00623BLA001' , '00702BLA00']
 
-names =  ['00147BLA001xQ' , '00150BLA001xQ' , '00537BLA001xQ' , '00006BLA004xQ' , '00043BLA001xQ' , '00170BLA006xQ' , '00241BLA001xQ' , '00813BLA008xQ' , '00034BLA003' , '00165BLA002' , '00192BLA001' , '00542BLA001' , '00622BLA001' , '00623BLA001' , '00702BLA00']
-
-samp_ids = dict(zip(ids, names))
-mock_id_lst = 'IssueID,SampleName\n' + '\n'.join(map(','.join, samp_ids.items()))
+samp_ids = dict(zip(ids, samps))
+mock_id_lst = 'IssueID,SampleName\n' + '\n'.join(map(','.join, zip(ids_strs, samps)))
 import random
 import string
 
@@ -20,7 +24,10 @@ def rand_string(size):
    return ''.join([random.choice(string.ascii_letters + string.digits) for n in range(size)])
 # using context manager for the belw instead
 # @patch("sh.gunzip", create=True,  side_effect=fake_gunzip)
-def big_test(sh_gunzip):
+#def big_test(sh_gunzip):
+def big_test():
+    indir =  Path('/foo/indir/')
+    rbs =   Path('/bar/outdir')
     with Patcher() as patcher:
         # access the fake_filesystem object via patcher.fs
         # # # Setup fake files 
@@ -29,6 +36,8 @@ def big_test(sh_gunzip):
         patcher.fs.create_dir(indir)
         patcher.fs.create_dir(rbs)
         # add some random dirs to rbs
+        
+        patcher.fs.create_dir(rbs / 'prentendoldsample')
         old_rbs_dirs = os.listdir(rbs)
         ftypes = ['R2', 'I1', 'R1', 'I2']
         for samp in samps:
@@ -42,29 +51,34 @@ def big_test(sh_gunzip):
         #def fake_gunzip(inpath, **kwargs):
             #assert kwargs.keys == ['_out']
         def fake_gunzip(inpath):
-            contents = open(inpath.read())) + '\nunzipped!'
+            contents = open(inpath).read() + '\nunzipped!'
             assert inpath.endswith('.gz')
             patcher.fs.create_file(inpath.rstrip('.gz'), contents=contents)
             
-        with patch('sh.gunzip', create=True, side_effect=fake_gunzip)
-            run_mycode(rbs, indir, in_sheet)
+        with patch('sh.gunzip', create=True, side_effect=fake_gunzip):
+            newsync.run(indir,rbs, in_sheet)
         
             # # # Assertions
-            new_dirs = Counter(old_rbs_dirs) - Counter(os.listdir(rbs)) 
-            assert new_dirs == Counter(samps)
+            new_dirs = Counter(os.listdir(rbs))  - Counter(old_rbs_dirs) 
+            eq_(new_dirs, Counter(ids_strs))
+            print os.listdir(indir)
+            print os.listdir(rbs)
             today = date.today().isoformat().replace('-','_')
-            for id, samp in samp_ids:
-               expected = map(lambda x: '{0}_{1}_{2}_{3}.fastq.gz'.format(id, x, today, junk), ftypes)
+            for id, samp in samp_ids.items():
+               expected = map(lambda x: '{0}_{1}_{2}_{3}.fastq'.format(id, x, junk, today), ftypes)
+               print os.listdir(rbs / str(id))
                for ftype, exp in zip(ftypes, expected):
-                   fp = rbs / dir / exp
-                   assert (os.path.exists(fp))
-                   exp_contents = open(indir / '{0}_{1}_{2}.fastq.gz'.format(samp, ftype, junk))
-                   assert open(fp).read() == exp_contents
+                   fp = rbs / str(id) / exp
+                   ok_(os.path.exists(fp), msg=fp +' not exist')
+                   exp_contents = open(indir / '{0}_{1}_{2}.fastq.gz'.format(samp, ftype, junk)).read() + '\nunzipped!'
+                   eq_(open(fp).read(), exp_contents)
        
     # duplicate dirs are impossible anyway, but still using Counter
-    
-def little_test():
+def rename_issue_date_test():
+  from newsync import rename_issue_date    
   name = '00001BLA001_S17_L002_I1_001.fastq.gz'
+  today = date.today().isoformat().replace('-','_')
   id_ = 2415
-  p = rename_issue_date(Path(name), '00001BLA001', id_)
+  p = rename_issue_date(Path('/rbs'),'00001BLA001', id_,  Path(name))
+  eq_(p, Path('/rbs/{id}/{id}_S17_L002_I1_001_{date}.fastq'.format(id=id_,date=today)))
 
